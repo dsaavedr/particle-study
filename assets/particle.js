226ae5e3f227d6bba55fb2849fd026975013bb27
Daniel Saavedra <danielsaavedram@hotmail.com>
2020-08-25 23:08:10 -0500

    Prticle object with internal values and update and show methods

---
+++ assets/particle.js
@@ -0,0 +1,24 @@
+class Particle {
+    constructor(pos, vel, r = 5) {
+        this.pos = pos;
+        this.acc = [0, 0];
+        this.vel = vel;
+        this.r = r;
+    }
+
+    applyForce(force) {
+        this.acc = force;
+    }
+
+    update() {
+        this.vel[0] += this.acc[0];
+        this.vel[1] += this.acc[1];
+        this.pos[0] += this.vel[0];
+        this.pos[1] += this.vel[1];
+        this.acc = 0;
+    }
+
+    show() {
+        point(this.pos[0], this.pos[1], this.r);
+    }
+}
