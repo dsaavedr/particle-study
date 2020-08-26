85cd3bfda6f209622839da7654eb314cd0ceaf2e
Daniel Saavedra <danielsaavedram@hotmail.com>
2020-08-25 22:36:36 -0500

    Added barebones animation structure

---
+++ master.js
@@ -0,0 +1,35 @@
+var canvas = document.getElementById('canvas'),
+    ctx = canvas.getContext('2d');
+
+var requestAnimationFrame = window.requestAnimationFrame || 
+    window.mozRequestAnimationFrame || 
+    window.webkitRequestAnimationFrame || 
+    window.msRequestAnimationFrame;
+
+function log(s) {
+  console.log(s);
+}
+
+function scale(num, in_min, in_max, out_min, out_max) {
+  return (num - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
+
+function init() {
+  WIDTH = window.innerWidth;
+  HEIGHT = window.innerHeight;
+    
+  canvas.setAttribute('width', WIDTH);
+  canvas.setAttribute('height', HEIGHT);
+  
+  ctx.clearRect(0, 0, WIDTH, HEIGHT);
+  ctx.beginPath();
+  ctx.fillStyle = 'black';
+  ctx.fillRect(0, 0, WIDTH, HEIGHT);
+  ctx.closePath();
+  
+  ani();
+}
+
+function ani() {
+  
+}
