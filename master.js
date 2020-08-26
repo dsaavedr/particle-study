1af8e36cc4b1d8267a7f450872b2bb23c198535c
Daniel Saavedra <danielsaavedram@hotmail.com>
2020-08-25 23:07:49 -0500

    Added a few auxiliary functions (should move them to seperate file) and animated particles and 'motion blur'

--- master.js
+++ master.js
@@ -1,3 +1,6 @@
+var particles = [],
+    gravity = 0.5;
+
 var canvas = document.getElementById('canvas'),
     ctx = canvas.getContext('2d');
 
@@ -14,6 +17,18 @@ function scale(num, in_min, in_max, out_min, out_max) {
   return (num - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 }
 
+function point(x, y, r){
+    ctx.beginPath();
+    ctx.arc(x, y, r, 0, 2 * Math.PI, true);
+    ctx.fill();
+    //ctx.stroke();
+    ctx.closePath();
+}
+
+function random(min = 0, max = 1) {
+    return Math.random() * (max - min) + min;
+}
+
 function init() {
   WIDTH = window.innerWidth;
   HEIGHT = window.innerHeight;
@@ -26,10 +41,31 @@ function init() {
   ctx.fillStyle = 'black';
   ctx.fillRect(0, 0, WIDTH, HEIGHT);
   ctx.closePath();
+
+  ctx.fillStyle = 'white';
   
   ani();
 }
 
 function ani() {
-  
+    ctx.save();
+    ctx.fillStyle = 'black';
+    ctx.globalAlpha = 0.1;
+    ctx.fillRect(0, 0, WIDTH, HEIGHT);
+    ctx.restore();
+    ctx.fillStyle = 'white';
+        
+    if (Math.random() < .15) {
+        particles.push(new Particle([Math.random() * WIDTH, HEIGHT], [0, -random(15, 25)]));
+    }
+    for (var i = 0; i < particles.length; i++) {
+        var p = particles[i];
+        p.applyForce([0, gravity]);
+        p.update();
+        p.show();
+    }
+
+    requestAnimationFrame(ani);
 }
+
+init();
